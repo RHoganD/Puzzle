54a8da6d0c9ce9308e0102842e1ab87df8ecb7a4
RHoganD <richpatric@hotmail.com>
2022-10-31 22:45:27 +0000

    add js code to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,159 @@
+// const rows = 3;
+// const columns = 5;
+
+// let currTile;
+// let otherTile;  
+
+// let turns = 0;
+
+// // let imgOrder = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15" ]; //name of the pictures
+// let imgOrder = ["01", "02", "03", "05", "04", "06", "07", "08", "09", "10", "12", "11", "13", "14", "15"];
+
+// window.onload = function() {
+//     for (let r=0; r < rows; r++) {
+//         for (let c=0; c < columns; c++) {
+
+//             //<img id="0-0" src="1.jpg">
+//             let tile = document.createElement("img");
+//             tile.id = r.toString() + "-" + c.toString();
+//             tile.src = imgOrder.shift() + ".jpg";
+                                   
+
+//             //DRAG FUNCTIONALITY EventListener
+//             tile.addEventListener("dragstart", dragStart);  //click an image to drag
+//             tile.addEventListener("dragover", dragOver);    //moving image around while clicked
+//             tile.addEventListener("dragenter", dragEnter);  //dragging image onto another one
+//             tile.addEventListener("dragleave", dragLeave);  //dragged image leaving anohter image
+//             tile.addEventListener("drop", dragDrop);        //drag an image over another image, drop the image
+//             tile.addEventListener("dragend", dragEnd);      //after drag drop, swap the two tiles
+
+
+
+//             document.getElementById("board").append(tile);
+
+           
+//         }
+//     }
+// }
+
+// function dragStart() {
+//     currTile = this; //this refers to the img tile being dragged
+// }
+
+// function dragOver(e) {
+//     e.preventDefault();
+// }
+
+// function dragEnter(e) {
+//     e.preventDefault();
+// }
+
+// function dragLeave() {
+
+// }
+
+// function dragDrop() {
+//     otherTile = this; //this refers to the img tile being dropped on
+// }
+
+// function dragEnd() {
+//     if (!otherTile.src.includes("01.jpg")) {
+//         return;
+//     }
+ 
+//     let currCoords = currTile.id.split("-"); //ex) "0-0" -> ["0", "0"]
+//     let r = parseInt(currCoords[0]);
+//     let c = parseInt(currCoords[1]);
+
+//     let otherCoords = otherTile.id.split("-");
+//     let r2 = parseInt(otherCoords[0]);
+//     let c2 = parseInt(otherCoords[1]);
+
+//     let moveLeft = r == r2 && c2 == c-1;
+//     let moveRight = r == r2 && c2 == c+1;
+
+//     let moveUp = c == c2 && r2 == r-1;
+//     let moveDown = c == c2 && r2 == r+1;
+
+//     let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+
+//    if (isAdjacent){
+//         let currImg = currTile.src;
+//         let otherImg = otherTile.src;
+
+//         currTile.src = otherImg;
+//         otherTile.src = currImg;
+
+//         turns +=1;
+//         document.getElementById("turns").innerText=turns;
+//    }
+
+// }
+
 const rows = 3;
 const columns = 5;
 
 let currTile;
-let otherTile;  
+let otherTile;
 
 let turns = 0;
 
-// let imgOrder = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15" ]; //name of the pictures
-let imgOrder = ["01", "02", "03", "05", "04", "06", "07", "08", "09", "10", "12", "11", "13", "14", "15"];
-
 window.onload = function() {
-    for (let r=0; r < rows; r++) {
-        for (let c=0; c < columns; c++) {
+    //initialize the 5x5 board
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
 
-            //<img id="0-0" src="1.jpg">
+            //<img>
             let tile = document.createElement("img");
-            tile.id = r.toString() + "-" + c.toString();
-            tile.src = imgOrder.shift() + ".jpg";
-                                   
+            tile.src = "./images/blank.jpg";
 
-            //DRAG FUNCTIONALITY EventListener
-            tile.addEventListener("dragstart", dragStart);  //click an image to drag
-            tile.addEventListener("dragover", dragOver);    //moving image around while clicked
-            tile.addEventListener("dragenter", dragEnter);  //dragging image onto another one
-            tile.addEventListener("dragleave", dragLeave);  //dragged image leaving anohter image
-            tile.addEventListener("drop", dragDrop);        //drag an image over another image, drop the image
-            tile.addEventListener("dragend", dragEnd);      //after drag drop, swap the two tiles
-
-
+            //DRAG FUNCTIONALITY
+            tile.addEventListener("dragstart", dragStart); //click on image to drag
+            tile.addEventListener("dragover", dragOver);   //drag an image
+            tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
+            tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
+            tile.addEventListener("drop", dragDrop);       //drop an image onto another one
+            tile.addEventListener("dragend", dragEnd);      //after you completed dragDrop
 
             document.getElementById("board").append(tile);
-
-           
         }
+    }
+
+    //pieces
+    let pieces = [];
+    for (let i=1; i <= rows*columns; i++) {
+        pieces.push(i.toString()); //put "1" to "25" into the array (puzzle images names)
+    }
+    pieces.reverse();
+    for (let i =0; i < pieces.length; i++) {
+        let j = Math.floor(Math.random() * pieces.length);
+
+        //swap
+        let tmp = pieces[i];
+        pieces[i] = pieces[j];
+        pieces[j] = tmp;
+    }
+
+    for (let i = 0; i < pieces.length; i++) {
+        let tile = document.createElement("img");
+        tile.src = "./images/" + pieces[i] + ".jpg";
+
+        //DRAG FUNCTIONALITY
+        tile.addEventListener("dragstart", dragStart); //click on image to drag
+        tile.addEventListener("dragover", dragOver);   //drag an image
+        tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
+        tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
+        tile.addEventListener("drop", dragDrop);       //drop an image onto another one
+        tile.addEventListener("dragend", dragEnd);      //after you completed dragDrop
+
+        document.getElementById("pieces").append(tile);
     }
 }
 
+//DRAG TILES
 function dragStart() {
-    currTile = this; //this refers to the img tile being dragged
+    currTile = this; //this refers to image that was clicked on for dragging
 }
 
 function dragOver(e) {
@@ -53,40 +169,20 @@ function dragLeave() {
 }
 
 function dragDrop() {
-    otherTile = this; //this refers to the img tile being dropped on
+    otherTile = this; //this refers to image that is being dropped on
 }
 
 function dragEnd() {
-    if (!otherTile.src.includes("01.jpg")) {
+    if (currTile.src.includes("blank")) {
         return;
     }
- 
-    let currCoords = currTile.id.split("-"); //ex) "0-0" -> ["0", "0"]
-    let r = parseInt(currCoords[0]);
-    let c = parseInt(currCoords[1]);
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+    currTile.src = otherImg;
+    otherTile.src = currImg;
 
-    let otherCoords = otherTile.id.split("-");
-    let r2 = parseInt(otherCoords[0]);
-    let c2 = parseInt(otherCoords[1]);
-
-    let moveLeft = r == r2 && c2 == c-1;
-    let moveRight = r == r2 && c2 == c+1;
-
-    let moveUp = c == c2 && r2 == r-1;
-    let moveDown = c == c2 && r2 == r+1;
-
-    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
-
-
-   if (isAdjacent){
-        let currImg = currTile.src;
-        let otherImg = otherTile.src;
-
-        currTile.src = otherImg;
-        otherTile.src = currImg;
-
-        turns +=1;
-        document.getElementById("turns").innerText=turns;
-   }
-
+    turns += 1;
+    document.getElementById("turns").innerText = turns;
 }
+
+
